823c8c1e2434851f52566e0a768f736f0bbf5215
milthoner <duvans427@gmail.com>
2024-03-15 16:10:00 -0500

    Login whith google next

---
+++ nexttauth-google-examples/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      Hola mudno
+    </div>
+  )
+}
+
+export default HomePage
---
+++ nexttauth-google-examples/src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { signIn, useSession, signOut } from "next-auth/react";
+
+function Navbar() {
+  const { data: session } = useSession();
+
+  return (
+    <nav className="bg-slate-900 flex items-center py-3 justify-between px-24   text-white">
+      <Link href="/">
+        <h1>NextGoogle</h1>
+      </Link>
+      {session?.user ? (
+        <div className="flex gap-x-2 items-center">
+          <Link href="/dashboard">Dashboard</Link>
+          <p>
+            {session.user.name} {session.user.email}
+          </p>
+          <img
+            src="{session.user.image}"
+            alt=""
+            className="2-10 h-10 rounded-full cursor-pointer"
+          />
+          <button
+            onClick={async () => {
+              await signOut({
+                callbackUrl:'/'
+              });
+            }}
+          >
+            LogOut
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={() => signIn()}
+          className="bg-sky-400 px-3 py-2 rounded"
+        >
+          Sign In
+        </button>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
